59fffb12e80f74f1bc8c29a8baa17a9523a10a64
Tuss4 <tuss4dbfn@gmail.com>
2013-09-17 05:19:01 -0500

    tweaking settings so everything runs on linux as well as windows

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -25,6 +25,19 @@ if socket.gethostname() == 'linuxmint':
         'PORT': '',                      # Set to empty string for default.
         }
     }
+elif socket.gethostname() == 'DreamMachine':
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'forum',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+        }
+    }
+elif 
 else:
     DATABASES = {'default': dj_database_url.config()}
 # Hosts/domain names that are valid for this site; required if DEBUG is False
