2e059d1886718dbc9c3a687da70e304d91bc7251
Tuss4 <tuss4dbfn@gmail.com>
2013-09-18 20:02:49 -0500

    admin tweak

--- forum/structure/admin.py
+++ forum/structure/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from structure.models import Category, SubCategory, Topic, Post
+from structure.models import Category, SubCategory, Topic, Post, PostCount
 
 admin.site.register(Category)
 admin.site.register(SubCategory)
 admin.site.register(Topic)
 admin.site.register(Post)
+admin.site.register(PostCount)
